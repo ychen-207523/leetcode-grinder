9a22120c105efd870a9601f06dae024ada337334
Yunfei Chen <ychen207523@gmail.com>
2024-12-23 18:17:30 -0500

    update test

--- test/test_send_LeetCode_daily_review.py
+++ test/test_send_LeetCode_daily_review.py
@@ -1,9 +1,10 @@
 import os
-import sys
 import unittest
-
+from dotenv import load_dotenv
 from src.lambda_handler.send_LeetCode_daily_review import lambda_handler
 
+# Load environment variables from .env file
+load_dotenv()
 
 class TestLambdaFunction(unittest.TestCase):
     def test_lambda_handler_execution(self):
@@ -14,9 +15,9 @@ class TestLambdaFunction(unittest.TestCase):
         # Call Lambda function directly
         response = lambda_handler(event, context)
 
+        # Validate the response
         self.assertEqual(response['statusCode'], 200)
         self.assertIn("Message sent successfully", response['body'])
-
 
 if __name__ == "__main__":
     unittest.main()
